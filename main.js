557c339e11cedccd8b2b1d53ff8362d5d1d6b343
Gona Choi <gorani831@gmail.com>
2025-09-08 11:44:36 +1000

    numberguess-1

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
-function meetAt(year, month, day) {
-  if (year && !month && !day) {
+let computerNum = 0;
 
-    return year + "년";
-  } else if (year && month && !day) {
-
-    return year + "년 " + month + "월";
-  } else if (year && month && day) {
-
-    return year + "/" + month + "/" + day;
-  }
+function randomNum() {
+  computerNum = Math.floor(Math.random()*100)+1
+  console.log("정답",computerNum)
 }
 
-console.log(meetAt(1992,11,5))
+randomNum()
+
+let numberInput = document.getElementById("number-input")
+let playButton = document.getElementById("play-button")
+let resultArea = document.getElementById("result-area")
+
+playButton.addEventListener("click", play)
+
+function play(){
+  let userValue = numberInput.value
+  if (computerNum > userValue){
+    resultArea.textContent = "Up"
+  } 
+  else if (computerNum < userValue){
+    resultArea.textContent = "Down"
+  }
+  else{
+    resultArea.textContent = "정답입니다"
+  }
+}
