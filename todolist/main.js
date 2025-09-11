1509f947c6c864e434336373f065aaac1a05d23a
Gona Choi <gorani831@gmail.com>
2025-09-12 02:28:12 +1000

    4end

--- todolist/main.js
+++ todolist/main.js
@@ -18,30 +18,35 @@ function render(){
     let resultHTML = ''
     for(let i=0;i<taskList.length; i++){
         if (taskList[i].isComplete == true){
-            resultHTML += `<div class="task">
+            resultHTML += `<div class="task task-done">
                     <div class = "task-done">${taskList[i].taskContent}</div>
                     <div>
-                        <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-                        <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+                        <button onclick="toggleComplete('${taskList[i].id}')"><i class="fa-solid fa-rotate-right"></i></button>
+                        <button onclick="deleteTask('${taskList[i].id}')"><i class="fa-solid fa-trash-can" style="color: #db490a;"></i></button>
                     </div>
                 </div>`
         }else{
             resultHTML += `<div class="task">
                     <div>${taskList[i].taskContent}</div>
                     <div>
-                        <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-                        <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+                        <button onclick="toggleComplete('${taskList[i].id}')"><i class="fa-solid fa-square-check" style="color: #8aea2a;"></i></button>
+                        <button onclick="deleteTask('${taskList[i].id}')"><i class="fa-solid fa-trash-can" style="color: #db490a;"></i></button>
                     </div>
                 </div>`
             }
-            document.getElementById("task-board").innerHTML = resultHTML
+            
 }
+document.getElementById("task-board").innerHTML = resultHTML
 }
 
 function toggleComplete(id){
     for(let i=0;i<taskList.length;i++){
         if(taskList[i].id == id){
-            taskList[i].isComplete = true
+            if(taskList[i].isComplete == true){
+                taskList[i].isComplete = false;
+            }else{
+                taskList[i].isComplete = true;
+            }
             break;
         }
     }
